edadb17cecceb6e49568b063e6711feb2259d85b
LEONARDOCHEBES <leonardochebes@outlook.es>
2023-01-03 01:56:09 -0300

    se hizo hasta validacion id mail
    bleh

--- backend/index.js
+++ backend/index.js
@@ -11,16 +11,23 @@ app.use(cors())
 database mongodb://localhost:27017
 ======================================*/
 const mongoose = require("mongoose")
+const URI = "mongodb://127.0.0.1:27017/loginMern"
 
-mongoose.connect("mongodb://localhost:27017/loginMern", {
+/*
+    //estas opciones dan error
+
+    const options ={
     useNewUrlParser : true,
-    useUnifieldTopology : true,
+    useCreateIndex: true,
+    useUnifieldTopology : true
 
-}).then(()=>{
-    console.log("conected good")
-}).catch((err)=>{
+}*/
+
+mongoose.connect(URI).then(
+    ()=>{console.log("conected good")}).catch((err)=>{
     console.log(err)
-})
+    console.log("PEPEEEEEEEEaaaaaaa")
+});
 
 
 /*+++++++++++++++++++++
@@ -37,7 +44,7 @@ const userSchema = new mongoose.Schema({
         unique : true,
     },
     password: String,
-    repassword: String,
+    repassword: String
 })
 
 const UserModel = new mongoose.model("UserModel", userSchema)
@@ -54,8 +61,23 @@ app.get("/", (req, res) => {
 
 app.post("/register", (req, res)=>{
     console.log(req.body)
+    const {firstName,lastName,email,password,repassword} =req.body
 
-
+    UserModel.findOne({email : email}, (err,user)=>{
+            if(user){
+                res.send({message : "Ya existe"})
+            }else{
+                const user = new UserModel ({
+                    firstName,
+                    lastName,
+                    email,
+                    password,
+                    repassword
+                })
+                user.save();
+                res.send({message : "Register"})
+            }
+    })
 
 })
 
